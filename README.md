e45e09c788018e4ce50a3826623d18829a191795
merry-mouse <potekh.anastasia@gmail.com>
2024-04-09 13:37:57 +0200

    add band_name_generator, gitignore and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Following 100 Days of Code: The Complete Python Pro Bootcamp
+
+[Link to the course on Udemy](https://www.udemy.com/course/100-days-of-code/)
---
+++ band_name_generator/main.py
@@ -0,0 +1,6 @@
+print("Hi! Nice to see you here!\nLet's generate a super cool band name for you!\n")
+
+city = input("What is the name of the city you were born?\n")
+pet = input("What is the name of your pet?\n")
+
+print(f"Theb super cool name for your band is: {city} {pet}.")
